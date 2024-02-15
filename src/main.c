e9995f8eeb8e747ef11acf53972940f6397d7d76
tiziaco <tiziano.iaco@gmail.com>
2024-02-15 18:04:15 +0100

    modify main

--- src/main.c
+++ src/main.c
@@ -6,14 +6,24 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 16:16:11 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/02/15 14:31:03 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/02/15 17:01:55 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minish.h"
+#include "../include/commands.h"
 
-int	main(int argc, char **argv)
+/* int	main(int argc, char **argv)
 {
-	/* adfgdf */
+	// adfgdf
+	return (0);
+} */
+
+extern char **environ;
+
+int	main(int argc, char **argv, char **envp)
+{
+	ft_pwd();
+	ft_cd("adsfsd");
+	ft_pwd();
 	return (0);
 }
