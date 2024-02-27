a2c126eef38c59e77ee83436324fa2071d4e82bf
Tiziano Iacovelli <tiacovel@c4b5c1.42berlin.de>
2024-02-27 19:00:07 +0100

    add background mode

--- src/main.c
+++ src/main.c
@@ -6,31 +6,80 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 16:16:11 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/02/26 17:35:07 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/02/27 18:25:14 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/commands.h"
 
-/* int	main(int argc, char **argv)
+static int	check_args(t_data *data, int argc, char **argv)
 {
-	// adfgdf
-	return (0);
-} */
+	if (argc != 1 && argc != 3)
+		return (sys_error(ARG_ERROR));
+	if (argc == 3)
+	{
+		data->background_mode = false;
+		if (!argv[1] || (argv[1] && ft_strcmp(argv[1], "-c") != 0))
+			return (sys_error(ARG_ERROR));
+		else if (!argv[2] || (argv[2] && argv[2][0] == '\0'))
+			return (sys_error(ARG_ERROR));
+	}
+	else
+		data->background_mode = true;
+	return (true);
+}
+
+void	init_foreground_mode(t_data *data)
+{
+	int	exit_code;
+
+	while (1)
+	{
+		/* set_signals_interactive();
+		set_signals_noninteractive(); */
+		data->line = readline(MSH_PROMPT);
+		if (parse_input(data) == OP_SUCCESS)
+			exit_code = execute_command(data);
+		else
+			exit_code = OP_FAIL;
+		free_data(data, false);
+	}
+}
+
+void	init_background_mode(t_data	*data, char *arg)
+{
+	char	**inputs;
+	int		exit_code;
+	int		i;
+
+	inputs = ft_split(arg, ';');
+	if (!inputs)
+		exit_shell(data, EXIT_FAILURE);
+	i = 0;
+	while (inputs[i])
+	{
+		data->line = ft_strdup(inputs[i]);
+		if (parse_user_input(data) == true)
+			exit_code = execute_command(data);
+		else
+			exit_code = OP_FAIL;
+		i++;
+		free_data(data, false);
+	}
+	free_double_pointer(inputs);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
-	char *command = "ls";
-	char *args[] = {command, "-", NULL};
-	/* ft_pwd();
-	ft_cd("adsfsd");
-	ft_pwd(); */
-	//ft_env(envp);
-	/* ft_export(&envp, "TEST_VAR", "xxxx");
-	ft_env(envp);
-	ft_unset(&envp, "TEST_VAR");
-	ft_env(envp); */
-	//heredoc("EOF");
-	execute_command(command, args, envp);
+	t_data	*data;
+
+	data = init_data(*envp);
+	if (!data)
+		return (EXIT_FAILURE);
+	if (data->background_mode)
+		init_background_mode(data, argv[2]);
+	else
+		init_foreground_mode(data);
+	//exit_shell(&data, exit_code);
 	return (0);
 }
