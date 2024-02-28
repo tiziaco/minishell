5ac41aff514b6783473d6d81ea337baf18c74374
Tiziano Iacovelli <tiacovel@c4b8c1.42berlin.de>
2024-02-28 17:40:22 +0100

    add clean pointer function

--- src/outils/memory_clean.c
+++ src/outils/memory_clean.c
@@ -6,11 +6,20 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 11:10:14 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/02/27 17:00:40 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:02:07 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/commands.h"
+
+void	free_pointer(void *ptr)
+{
+	if (ptr != NULL)
+	{
+		free(ptr);
+		ptr = NULL;
+	}
+}
 
 void	free_double_pointer(char **str)
 {
