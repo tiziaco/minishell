236a3e766ab214b157cab2b17ac916163f187cdd
Tiziano Iacovelli <tiacovel@c4b8c1.42berlin.de>
2024-02-28 17:34:43 +0100

    modify headhers

--- include/commands.h
+++ include/commands.h
@@ -6,28 +6,22 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 15:03:26 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/02/27 16:54:32 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:04:02 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minish.h"
 
-void	execute_command(char *command, char **arguments, char **envp);
+int	execute_command(t_data *data);
 
 /* Built-in commnads */
-void	ft_echo(char **str);
-void	ft_cd(char *str);
-void	ft_pwd(void);
-int		ft_export(char ***envp, const char *name, const char *value);
-int		ft_unset(char ***envp, const char *name);
-int		ft_exit(char **str);
-void	ft_env(char **envp);
+int		ft_echo(char **str);
+int		ft_cd(char *str);
+int		ft_pwd(void);
+int		ft_export(t_data *data, char **args);
+int		ft_unset(char ***envp, char **args);
+int		ft_exit(void);
+int		ft_env(char ***envp);
 
 /* Redirections */
 void	heredoc(char *delimeter);
-
-/* Outils */
-char	*ft_strcat(char *dest, char *src);
-char	*ft_strcpy(char *dest, char *src);
-int		ft_strcmp(char *s1, char *s2);
-void	free_double_pointer(char **str);
--- include/minish.h
+++ include/minish.h
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 16:12:13 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/02/27 18:38:59 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:27:25 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,22 @@
 # define TMP_FILENAME "/home/tiacovel/core_curriculum/minishell/heredoc_tmp.tmp"
 
 /* Error codes */
-# define OP_SUCCESS	1
-# define OP_FAIL	-1
-# define MEM_ERROR  2
-# define ARG_ERROR  3
+# define OP_SUCCESS		1
+# define OP_FAIL		-1
+# define MEM_ERROR  	2
+# define ARG_ERROR  	3
+# define CMD_ERROR		4
+# define INVALID_KEY	10
+# define INVALID_PARM	11
 
 /* Struct / typedef / enum */
 typedef struct termios	t_term;
+
+typedef struct s_cmd
+{
+	char	*command;
+	char	**args;
+}	t_cmd;
 
 typedef struct s_data
 {
@@ -49,14 +58,29 @@ typedef struct s_data
 	char	*line;
 	char	***envp;
 	char	*token;
+	t_cmd	*cmd;
 }	t_data;
 
 /* Initialization functions */
 t_data	*init_data(char **envp);
 
-/* Utility functions */
+/* Environment functions */
+int		search_key(char ***envp, char *key);
+int		get_env_size(char **envp);
+char	*get_env_var(char *key, char *val);
+char	**copy_environment(char **envp, int env_size);
+int		set_var(t_data *data, char *key, char *val);
+int		remove_var(t_data *data, int key_index);
 
 /* Error handlers */
 int		sys_error(int err_code);
+int		cmd_error(int err_code);
+
+/* Outils */
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strcpy(char *dest, char *src);
+int		ft_strcmp(char *s1, char *s2);
+void	free_pointer(void *ptr);
+void	free_double_pointer(char **str);
 
 #endif
